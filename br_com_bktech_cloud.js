6994de2956c04dea487c5c08613a8b017b12b700
Fabio Soares Schmidt <fabio@improve.inf.br>
2021-02-13 21:55:07 -0300

    Update br_com_bktech_cloud.js

--- br_com_bktech_cloud.js
+++ br_com_bktech_cloud.js
@@ -26,7 +26,7 @@ var BKTechLink = br_com_bktech_cloud_HandlerObject;
 BKTechLink.prototype.init = function() {
 try {   
 var zimletInstance = appCtxt._zimletMgr.getZimletByName('br_com_bktech_cloud').handlerObject;
-document.getElementById('skin_container_refresh').insertAdjacentHTML('beforebegin',"<div title=\"Abrir Site\" onclick=\"window.open('https://nuvem.bktech.com.br/')\" style=\"float:left; background-image:url('"+zimletInstance.getResource('cloud-computing.png')+"');background-repeat:no-repeat;height:16px;width:16px;cursor:pointer\"></div>");  
+document.getElementById('skin_container_refresh').insertAdjacentHTML('beforebegin',"<div title=\"Abrir Site\" onclick=\"window.open('https://www.bktech.com.br/')\" style=\"float:left; background-image:url('"+zimletInstance.getResource('cloud-computing.png')+"');background-repeat:no-repeat;height:16px;width:16px;cursor:pointer\"></div>");  
 } catch(err)
 {
 //read email in a new window
